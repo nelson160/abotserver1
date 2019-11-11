afa4f1c6411a8b67240983b6213fa7b28503a94b
nelson160 <nilsholte@hotmail.de>
2019-11-11 18:08:01 +0100

    server.py update:Ausgabe Satz

--- server.py
+++ server.py
@@ -13,18 +13,17 @@ def index():
   data = json.loads(request.get_data().decode('utf-8'))
 
   # FETCH THE CRYPTO NAME
-  crypto_name = data['conversation']['memory']['crypto']['raw']
+  crypto_name = data['source']
   crypto_ticker = crypto_name.upper()
 
   # FETCH BTC/USD/EUR PRICES
-  r = requests.get("https://min-api.cryptocompare.com/data/price?fsym="+crypto_ticker+"&tsyms=BTC,USD,EUR")
+  # r = requests.get("https://min-api.cryptocompare.com/data/price?fsym="+crypto_ticker+"&tsyms=BTC,USD,EUR")
 
   return jsonify(
     status=200,
     replies=[{
       'type': 'text',
-      'content': 'The price of %s is :\n%f BTC, \n%f USD, and \n%f EUR.' % (crypto_ticker, r.json()['BTC'], r.json()['USD'], r.json()['EUR'])
-    }]
+      'content': 'Satz: %s ' % (crypto_ticker)
   )
 
 @app.route('/errors', methods=['POST'])
