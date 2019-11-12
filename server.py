e958482c34ded1a855ff431f8f55ad8abd4553c0
nelson160 <nilsholte@hotmail.de>
2019-11-12 11:38:52 +0100

    server.py update

--- server.py
+++ server.py
@@ -13,7 +13,7 @@ def index():
   data = json.loads(request.get_data().decode('utf-8'))
 
   # FETCH THE CRYPTO NAME
-  crypto_name = data['nlp']['conversation']['source']
+  crypto_name = data['nlp']['source']
   crypto_ticker = crypto_name.upper()
 
   # FETCH BTC/USD/EUR PRICES
