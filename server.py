ed49935856e22fa9abb617f49addac4739537f53
nelson160 <nilsholte@hotmail.de>
2019-11-13 15:12:38 +0100

    First server.py commit

--- server.py
+++ server.py
@@ -1,15 +1,7 @@
 from flask import Flask, request, jsonify
 import json
 import requests
-import keras
-from keras.preprocessing.sequence import pad_sequences
-import re
-import string
-import pandas as pd
-import numpy as np
-import tensorflow as tf
-import pickle
-
+import os
 
 app = Flask(__name__)
 port = int(os.environ.get('PORT', 33507))
@@ -20,16 +12,9 @@ def index():
   print(port)
   data = json.loads(request.get_data().decode('utf-8'))
 
-  # FETCH THE CRYPTO NAME.
+  # FETCH THE CRYPTO NAME
   crypto_name = data['nlp']['source']
   crypto_ticker = crypto_name.upper()
-
-  model_path = 'paper_cnn_gru_drop02.h5'
-  model = tf.keras.models.load_model(model_path)
-  predi = model.predict(crypto_ticker)
-  labels = ['negativ', 'neutral', 'positiv']
-  #print(predi, labels[np.argmax(predi)])
-  sentiment = labels[np.argmax(predi)]
 
   # FETCH BTC/USD/EUR PRICES
   #r = requests.get("https://min-api.cryptocompare.com/data/price?fsym="+crypto_ticker+"&tsyms=BTC,USD,EUR")
@@ -38,7 +23,7 @@ def index():
     status=200,
     replies=[{
       'type': 'text',
-      'content': 'Der Sentiment %s ' % (sentiment)
+      'content': 'Der Sentiment %s ' % (crypto_ticker)
     }]
   )
 
