3ae32393972f0ab34dc0aa8a184adef6d49ab901
nelson160 <nilsholte@hotmail.de>
2019-11-13 17:22:48 +0100

    First server.py commit

--- server.py
+++ server.py
@@ -1,35 +1,155 @@
 from flask import Flask, request, jsonify
+from keras.preprocessing.sequence import pad_sequences
+import re
+import string
+import numpy as np
+import tensorflow as tf
+import pickle
 import json
-import requests
-import os
+
+
 
 app = Flask(__name__)
 port = int(os.environ.get('PORT', 33507))
-print(port)
+
+
+chat_path = r"data/chat.json"
+model_path = 'paper_cnn_gru_drop02.h5'
+tokenizer_path =  "tokenizer.pickle"
+path_stopwords = r"data/stopwords_chatbot.txt"
+
+maxlen = 27
+
+#def load_json():
+ #   with open(chat_path) as json_file:
+  #      data = json.load(json_file)
+   #     for p in data['chat']:
+    #        txt = p['text']
+    #return txt
+
+
+
+def preprocess_text(sen):
+    sentence = sen
+    # Entfernt Punktuationen, behält Bindestriche !
+
+    remove = string.punctuation
+    remove = remove.replace("-", "")  # keine Bindestriche entfernen
+    pattern = r"[{}]".format(remove)  # Pattern erstellen
+    sentence = re.sub(pattern, "", sentence)
+
+    # Entfernt Zahlen
+    sentence = re.sub('[0-9]', '', sentence)
+
+    # Entfernt einzelne Buchstaben
+    sentence = re.sub(r"\s+[a-zA-Z]\s+", ' ', sentence)
+
+    # Entfernt mehrere Spaces
+    sentence = re.sub(r'\s+', ' ', sentence)
+
+    # Wandelt Großbuchstaben in Kleinbuchstaben um
+    sentence = sentence.lower()
+
+    return sentence
+
+
+def load_stopwords():
+    stopwordslist = open(path_stopwords, "r", encoding = "utf-8")
+    stop = stopwordslist.read()
+    stop = stop.split(', ')
+    return stop
+
+
+def load_models():
+    loaded_model = tf.keras.models.load_model(model_path)
+
+    with open(tokenizer_path, 'rb') as handle:
+        loaded_tokenizer = pickle.load(handle)
+
+    return loaded_model, loaded_tokenizer
+
+def do_prediction(txt, loaded_model, loaded_tokenizer, stop):
+    preprocessed = preprocess_text(txt)
+    without_stop = ([word for word in preprocessed.split() if word not in (stop)])
+    sentences_token = loaded_tokenizer.texts_to_sequences([without_stop])
+    sentences_pad = pad_sequences(sentences_token, padding='post', maxlen=maxlen)
+    sentences_pad
+    predi = loaded_model.predict(sentences_pad)
+    labels = ['negativ', 'neutral', 'positiv']
+    sentiment = labels[np.argmax(predi)]
+    return sentiment
+
+
+def save_json(txt):
+    json_output = {
+        'model': model_path,
+        'text': txt,
+        'sentiment': sentiment
+    }
+
+    with open('data/sentiment.json', 'w', encoding="utf-8") as json_file:
+        json.dump(json_output, json_file, ensure_ascii=False)
+
+
+txt = "Das ist schlecht!"#load_json()
+stop = load_stopwords()
+loaded_model, loaded_tokenizer = load_models()
+sentiment = do_prediction(txt, loaded_model, loaded_tokenizer, stop)
+save_json(txt)
+print("Der Sentimentwert wurde in sentiment.json abgespeichert")
+print(txt)
+
+
+@app.route("/mask")
+def index():
+    return '''
+        <html>
+            <body>
+                <form action= "http://localhost:5000/predict" method = "post">
+                    <p>Text:</p>
+                    <p><input type = "text" name ="name" /></p>
+                    <p><input type = "submit" value ="Sentiment Analyse" /></p>
+                </form>
+            </body>
+        </html>
+    '''
+
+@app.route("/predict", methods=['POST', 'GET'] )
+def predict():
+    name = ""
+    if request.method == 'POST':
+        name = request.form['name']
+    else:
+        name = request.args.get('name')
+
+    return name + txt + " Sentimentwert: " + sentiment
+
 
 @app.route('/', methods=['POST'])
-def index():
-  print(port)
-  data = json.loads(request.get_data().decode('utf-8'))
+def bot():
+    data = json.loads(request.get_data())
 
-  # FETCH THE CRYPTO NAME
-  crypto_name = data['nlp']['source']
-  crypto_ticker = crypto_name.upper()
+    bot_chat = data['nlp']['source']
+    print(type(bot_chat))
 
-  # FETCH BTC/USD/EUR PRICES
-  #r = requests.get("https://min-api.cryptocompare.com/data/price?fsym="+crypto_ticker+"&tsyms=BTC,USD,EUR")
+    stop = load_stopwords()
+    loaded_model, loaded_tokenizer = load_models()
+    sentiment = do_prediction(bot_chat, loaded_model, loaded_tokenizer, stop)
+    output_Text = "Ich sehe Ihre Stimmung ist: " + sentiment
+    print(bot_chat)
 
-  return jsonify(
-    status=200,
-    replies=[{
-      'type': 'text',
-      'content': 'Der Sentiment %s ' % (crypto_ticker)
-    }]
-  )
+    #r = json.loads()
 
-@app.route('/errors', methods=['POST'])
-def errors():
-  print(json.loads(request.get_data()))
-  return jsonify(status=200)
+    return jsonify(
+        status=200,
+        replies=[{
+            'type': 'text',
+            'content': output_Text
+        }]
+    )
 
-app.run(port=port, host="0.0.0.0")
+app.run(port=port)
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
